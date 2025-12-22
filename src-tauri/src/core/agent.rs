6a98e55e5a9573ebd58a6ca1026eca99e5c58dd4
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2025-12-22 15:15:30 +0200

    fixed agent resource dir path

--- src-tauri/src/core/agent.rs
+++ src-tauri/src/core/agent.rs
@@ -2,16 +2,33 @@ use std::process::Command;
 use tauri::{AppHandle, Manager};
 
 pub fn spawn_agent(app: &AppHandle) -> Result<(), String> {
-    let resource_dir = app.path().resource_dir().map_err(|e| e.to_string())?;
 
     #[cfg(target_os = "windows")]
-    let agent_path = resource_dir.join("windows/agent.exe");
+    let resource_path = "windows/agent.exe";
 	
 	#[cfg(target_os = "macos")]
-    let agent_path = resource_dir.join("macos/agent");
+    let resource_path = "macos/agent";
 	
 	#[cfg(target_os = "linux")]
-    let agent_path = resource_dir.join("linux/agent");
+    let resource_path = "linux/agent";
+
+	let agent_path = match app.path().resolve(resource_path, tauri::path::BaseDirectory::Resource) {
+		Ok (path) if path.exists() => {
+			path
+		},
+		_ => {
+
+			let path = std::env::current_dir()
+				.map_err(|e| e.to_string())?
+				.join("resources")
+				.join(resource_path);
+            
+            if !path.exists() {
+                return Err(format!("Agent not found in either location. Tried:\n- Resource dir\n- {:?}", path));
+            }
+            path
+		}
+	};
 
 	Command::new(agent_path)
 		.spawn()
